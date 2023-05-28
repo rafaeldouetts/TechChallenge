6a8d9ffa07b5ef5b1b82a17572b030fa5b6d56d6
Jair do Nascimento Junior <jair_nj@hotmail.com>
2023-05-27 23:16:07 -0300

    Adição de retorno para upload do Blob e adição do método GetSpecific que retorna sastoken com 1 hora de expiração

--- TechChallengeMinimalApi/Program.cs
+++ TechChallengeMinimalApi/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Reflection.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -44,12 +47,44 @@ app.MapPost("/upload", async (IFormFile formFile) => // (IFormFile file) =>
         // Faz upload do arquivo para o blob
         var blob = container.GetBlockBlobReference(blobName);
         await blob.UploadFromByteArrayAsync(fileBytes, 0, fileBytes.Length);
-        Results.NoContent();
+
+         // Obtém a URL de acesso ao blob
+         //var blobUrl = blob.Uri.ToString();
+         return Results.Ok(blobName);
     }
     catch (Exception ex)
     {
-        Results.NotFound(ex);
+        return Results.NotFound(ex);
     }
 }).Accepts<FormFile>("multipart/form-data");
+
+app.MapGet("/GetSpecific", async (string blobName) => 
+{
+    var containerName = "filescontainer";
+    var connectionString = configuration.GetConnectionString("blobstorage");
+    var storageAccount = CloudStorageAccount.Parse(connectionString);
+
+    // Cria um objeto CloudBlobClient a partir da conta de armazenamento
+    var blobClient = storageAccount.CreateCloudBlobClient();
+
+    // Cria o contêiner caso ele não exista
+    var container = blobClient.GetContainerReference(containerName);
+    await container.CreateIfNotExistsAsync();
+
+    // Cria a referencia do Blob
+    var blob = container.GetBlobReference(blobName);
+
+    // Obtém o SAS Token para o blob
+    var sasToken = blob.GetSharedAccessSignature(new SharedAccessBlobPolicy
+    {
+        SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddHours(1), // Define o tempo de expiração do SAS Token
+        Permissions = SharedAccessBlobPermissions.Read // Define as permissões do SAS Token (somente leitura neste caso)
+    });
+
+    // Constrói a URL do blob com o SAS Token
+    var blobUrlWithSas = blob.Uri + sasToken;
+
+    return Results.Ok(blobUrlWithSas); 
+});
 
 app.Run();
