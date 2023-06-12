f3bd1469ff1bcf033074d1ac0e72d2deef6283fa
Jair do Nascimento Junior <jair_nj@hotmail.com>
2023-06-11 22:12:17 -0300

    Criação do projeto TechChallenge.Identity e ajuste de nomenclatuda da Minimal API

---
+++ TechChallenge.Identity/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechChallenge.Identity.Data;
+public class ApplicationContext : IdentityDbContext<IdentityUser> ///Essa herança nos ajuda a implementar as funcionalidades do Identity
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
