a6e5c52d12eb86441aeba8154df9f65e0a59d183
Mateus Vieira da Assunção <mateusvassuncao@gmail.com>
2023-06-12 23:12:10 -0300

    Manage COntroller

--- TechChallengeWeb/Controllers/HomeController.cs
+++ TechChallengeWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TechChallengeWeb.Data;
 using TechChallengeWeb.Models;
 
 namespace TechChallengeWeb.Controllers
@@ -7,10 +8,24 @@ namespace TechChallengeWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPublicacaoRepository _publicacaoRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IPublicacaoRepository publicacaoRepository)
         {
             _logger = logger;
+            _publicacaoRepository = publicacaoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Publicacoes()
+        {
+
+            Usuario usuario = new Usuario("Mateus", "mateus@email.com");
+            Foto foto = new Foto(true, "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png");
+            Publicacao publicacao = new Publicacao("Test publicacao", usuario, "www.com", foto);
+            IEnumerable<Publicacao> viewModel = _publicacaoRepository.GetPublicacoes(usuario);
+            return View(viewModel);
+
         }
 
         public IActionResult Index()
