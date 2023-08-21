f14aa04457ec463c885d69dbaebef9708273c7cc
rafaeldouetts <r_santos07@outlook.com>
2023-08-20 23:15:31 -0300

    gerar a pagina swagger para teste em ambiente de producao

--- TechChallenge.Identity/Program.cs
+++ TechChallenge.Identity/Program.cs
@@ -54,11 +54,11 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
